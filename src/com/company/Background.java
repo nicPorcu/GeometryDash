e2a315f81c08b049b5fe38a24155877ab767658e
kevinX264 <Kevin1253416333>
2017-05-26 13:04:26 -0700

    cool

--- src/com/company/Background.java
+++ src/com/company/Background.java
@@ -220,7 +220,7 @@ public class Background {
         }
         for (int i= 0; i< numPillars; i++)
         {
-            pillars.get(i).shiftLeft(groundSpd);//check==cks
+            pillars.get(i).shiftLeft(groundSpd);//chs
         }
         for (int i= 0; i< numPortals; i++)
         {
